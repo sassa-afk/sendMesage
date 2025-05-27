ee361cf798d3a2678611756c389f555f097af006
Samuel Souto dos Santos <135268502+sassa-afk@users.noreply.github.com>
2025-05-27 17:51:34 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -27,8 +27,8 @@ app.post( "/mail/sendMain" , async (req , res ) =>{
 	try{
 		
 		let retorno  = objMail.sendMailLerSimple(hst , port , secure , user , pass , mailTo  , titulo , msgHTML , msgText);
-console.log(">>>>>>>>>>>>> "+ retorno )
- 		      return res.status(200).json({ message : retorno });
+              console.log(">>>>>>>>>>>>> "+ retorno )
+ 		      return res.status(200).json({ mesage : retorno });
 
 	}catch(err){
 		return res.status(500).json({ mesage : `Erro na execução do processo ${err.message}`});
