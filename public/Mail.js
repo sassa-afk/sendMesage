2119cd84467cf1fb25b0f43bf70a7a7c3932ac62
Samuel Souto dos Santos <135268502+sassa-afk@users.noreply.github.com>
2025-05-27 18:18:00 -0300

    Update Mail.js

--- public/Mail.js
+++ public/Mail.js
@@ -44,7 +44,7 @@ class Mail {
        // let retorno =   `Email sent successfully at ${ def.dateFormat() } | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Message ID: ${response.messageId}` ;
              let retorno =   `Email sent successfully at ${ def.dateFormat() } | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Message ID: ${response.messageId}` ;
 
-       return { log : retorno } ;
+       return { 'log' : retorno } ;
 
     } catch (er) {
 
@@ -52,7 +52,7 @@ class Mail {
 
       // let retorno = `log`: `Email sending failed at ${ def.dateFormat() } | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Error: ${err.message}`  ;
        let retorno =   `Email sending failed at ${def.dateFormat} | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Error: ${err.message}` ;
-       return { log : retorno } ;
+       return { 'log' : retorno } ;
 
     }
   }
