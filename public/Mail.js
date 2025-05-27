0fd7e8735aaf694470400884bfd4a3de3f763ebc
Samuel Souto dos Santos <135268502+sassa-afk@users.noreply.github.com>
2025-05-27 14:08:48 -0300

    Update Mail.js

--- public/Mail.js
+++ public/Mail.js
@@ -38,15 +38,17 @@ class Mail {
 
       });
 
-      console.log(`Email sent successfully at ${def.dateFormat} | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Message ID: ${response.messageId}`);
-       return `Email sent successfully at ${def.dateFormat} | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Message ID: ${response.messageId}` ;
+
+ 
+      console.log(`Email sent successfully at ${ def.dateFormat() } | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Message ID: ${response.messageId}`);
+       return `Email sent successfully at ${ def.dateFormat() } | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Message ID: ${response.messageId}` ;
 
     } catch (er) {
 
-console.error(`Email sending failed at ${def.dateFormat} | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Error: ${err.message}`);
+console.error(`Email sending failed at ${ def.dateFormat() } | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Error: ${err.message}`);
 
 
-       return  `Email sending failed at ${def.dateFormat} | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Error: ${err.message}` ;
+       return  `Email sending failed at ${ def.dateFormat() } | From: ${user} | To: ${mailTo} | Subject: "${titulo}" | Error: ${err.message}` ;
 
     }
   }
